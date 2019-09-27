dff2fd72d752158e29c5905fce5eecd661936f60
Freedom <40904131+ffreedomm@users.noreply.github.com>
2019-09-27 20:34:36 +0800

    修改

--- src/components/page/Dashboard.vue
+++ src/components/page/Dashboard.vue
@@ -2,12 +2,12 @@
     <div>
         <el-divider content-position="left">整体情况监控</el-divider>
         <el-row :gutter="20">
-            <el-col :span="24">
+            <el-col :span="12">
                 <el-row :gutter="20" class="mgb20">
-                    <el-col :span="4">
+                    <el-col :span="8">
                         <el-card
                             shadow="always"
-                            :body-style="{padding: '0px','background-color': 'aqua'}"
+                            :body-style="{padding: '0px','background-color': 'lightskyblue'}"
                         >
                             <div slot="header" class="clearfix">
                                 <div
@@ -21,7 +21,7 @@
                             </div>
                         </el-card>
                     </el-col>
-                    <el-col :span="4">
+                    <el-col :span="8">
                         <el-card
                             shadow="hover"
                             :body-style="{padding: '0px','background-color': '#94d6da'}"
@@ -38,7 +38,7 @@
                             </div>
                         </el-card>
                     </el-col>
-                    <el-col :span="4">
+                    <el-col :span="8">
                         <el-card
                             shadow="hover"
                             :body-style="{padding: '0px','background-color': '#dbce8f'}"
@@ -55,7 +55,9 @@
                             </div>
                         </el-card>
                     </el-col>
-                    <el-col :span="4">
+                </el-row>
+                <el-row :gutter="20" class="mgb20">
+                    <el-col :span="8">
                         <el-card
                             shadow="hover"
                             :body-style="{padding: '0px','background-color': '#d3d7d4'}"
@@ -73,7 +75,7 @@
                             </div>
                         </el-card>
                     </el-col>
-                    <el-col :span="4">
+                    <el-col :span="8">
                         <el-card
                             shadow="hover"
                             :body-style="{padding: '0px','background-color': '#f173ac'}"
@@ -90,7 +92,7 @@
                             </div>
                         </el-card>
                     </el-col>
-                    <el-col :span="4">
+                    <el-col :span="8">
                         <el-card
                             shadow="hover"
                             :header-style="{color:'#f3704b'}"
@@ -110,13 +112,19 @@
                     </el-col>
                 </el-row>
             </el-col>
+            <el-col :span="12">
+                <div id="myChart" :style="{width: '500', height: '290px'}"></div>
+            </el-col>
         </el-row>
         <el-divider></el-divider>
+         <!-- <el-row :gutter="20">
+            
+           <el-col :span="12">
+                 <div id="myChart2" :style="{width: '500', height: '380px'}"></div>
+             </el-col>
+        </el-row> -->
         <el-row :gutter="20">
-            <el-col :span="12">
-                <div id="myChart" :style="{width: '500', height: '380px'}"></div>
-            </el-col>
-            <el-col :span="12">
+            <el-col :span="24">
                 <div id="myChart2" :style="{width: '500', height: '380px'}"></div>
             </el-col>
         </el-row>
@@ -129,79 +137,203 @@ export default {
     name: 'dashboard',
     data() {
         return {
-            name: localStorage.getItem('ms_username'),
-            todoList: [
-                {
-                    title: '今天要修复100个bug',
-                    status: false
-                },
-                {
-                    title: '今天要修复100个bug',
-                    status: false
-                },
-                {
-                    title: '今天要写100行代码加几个bug吧',
-                    status: false
-                },
-                {
-                    title: '今天要修复100个bug',
-                    status: false
-                },
-                {
-                    title: '今天要修复100个bug',
-                    status: true
-                },
-                {
-                    title: '今天要写100行代码加几个bug吧',
-                    status: true
-                }
-            ],
-            data: [
-                {
-                    name: '2018/09/04',
-                    value: 1083
-                },
-                {
-                    name: '2018/09/05',
-                    value: 941
-                },
-                {
-                    name: '2018/09/06',
-                    value: 1139
-                },
-                {
-                    name: '2018/09/07',
-                    value: 816
-                },
-                {
-                    name: '2018/09/08',
-                    value: 327
-                },
-                {
-                    name: '2018/09/09',
-                    value: 228
-                },
-                {
-                    name: '2018/09/10',
-                    value: 1065
-                }
-            ],
-            options: {
-                title: '最近七天每天的用户访问量',
-                showValue: false,
-                fillColor: 'rgb(45, 140, 240)',
-                bottomPadding: 30,
-                topPadding: 30
-            },
-            options2: {
-                title: '最近七天用户访问趋势',
-                fillColor: '#FC6FA1',
-                axisColor: '#008ACD',
-                contentColor: '#EEEEEE',
-                bgColor: '#F5F8FD',
-                bottomPadding: 30,
-                topPadding: 30
-            }
+            "electricityTotalList": [{
+			"createTime": "2019-8-28-19-59-52",
+			"electricity": 0.0,
+			"power": 0.0,
+			"voltage": 0.0
+		}, {
+			"createTime": "2019-8-30-10-6-12",
+			"electricity": 0.0,
+			"power": 0.0,
+			"voltage": 0.0
+		}, {
+			"createTime": "2019-9-1-0-12-31",
+			"electricity": 0.0,
+			"power": 0.0,
+			"voltage": 0.0
+		}, {
+			"createTime": "2019-9-2-14-18-50",
+			"electricity": 0.0,
+			"power": 0.0,
+			"voltage": 0.0
+		}, {
+			"createTime": "2019-9-4-4-25-10",
+			"electricity": 0.0,
+			"power": 0.0,
+			"voltage": 0.0
+		}, {
+			"createTime": "2019-9-5-18-31-29",
+			"electricity": 0.0,
+			"power": 0.0,
+			"voltage": 0.0
+		}, {
+			"createTime": "2019-9-7-8-37-48",
+			"electricity": 0.0,
+			"power": 0.0,
+			"voltage": 0.0
+		}, {
+			"createTime": "2019-9-8-22-44-8",
+			"electricity": 0.0,
+			"power": 0.0,
+			"voltage": 0.0
+		}, {
+			"createTime": "2019-9-10-12-50-27",
+			"electricity": 0.0,
+			"power": 0.0,
+			"voltage": 0.0
+		}, {
+			"createTime": "2019-9-12-2-56-46",
+			"electricity": 0.0,
+			"power": 0.0,
+			"voltage": 0.0
+		}, {
+			"createTime": "2019-9-13-17-3-6",
+			"electricity": 0.0,
+			"power": 0.0,
+			"voltage": 0.0
+		}, {
+			"createTime": "2019-9-15-7-9-25",
+			"electricity": 0.0,
+			"power": 0.0,
+			"voltage": 0.0
+		}, {
+			"createTime": "2019-9-16-21-15-44",
+			"electricity": 0.0,
+			"power": 0.0,
+			"voltage": 0.0
+		}, {
+			"createTime": "2019-9-18-11-22-4",
+			"electricity": 0.0,
+			"power": 0.0,
+			"voltage": 0.0
+		}, {
+			"createTime": "2019-9-20-1-28-23",
+			"electricity": 0.0,
+			"power": 0.0,
+			"voltage": 0.0
+		}, {
+			"createTime": "2019-9-21-15-34-42",
+			"electricity": 88.18316580749493,
+			"power": 0.0,
+			"voltage": 0.0
+		}, {
+			"createTime": "2019-9-23-5-41-1",
+			"electricity": 27.611924328376144,
+			"power": 0.0,
+			"voltage": 0.0
+		}, {
+			"createTime": "2019-9-24-19-47-21",
+			"electricity": 30.446782748721446,
+			"power": 0.0,
+			"voltage": 0.0
+		}, {
+			"createTime": "2019-9-26-9-53-40",
+			"electricity": 556.4789419102278,
+			"power": 26590.533078292417,
+			"voltage": 373.10070114440646
+		}, {
+			"createTime": "2019-9-27-23-59-59",
+			"electricity": 0.0,
+			"power": 0.0,
+			"voltage": 0.0
+		}],
+        "powerTotalList": [{
+			"endTime": "2019-8-30-10-6-12",
+			"kwh": 0.0,
+			"power": 0.0,
+			"startTime": "2019-8-28-19-59-52"
+		}, {
+			"endTime": "2019-9-1-0-12-31",
+			"kwh": 0.0,
+			"power": 0.0,
+			"startTime": "2019-8-30-10-6-12"
+		}, {
+			"endTime": "2019-9-2-14-18-50",
+			"kwh": 0.0,
+			"power": 0.0,
+			"startTime": "2019-9-1-0-12-31"
+		}, {
+			"endTime": "2019-9-4-4-25-10",
+			"kwh": 0.0,
+			"power": 0.0,
+			"startTime": "2019-9-2-14-18-50"
+		}, {
+			"endTime": "2019-9-5-18-31-29",
+			"kwh": 0.0,
+			"power": 0.0,
+			"startTime": "2019-9-4-4-25-10"
+		}, {
+			"endTime": "2019-9-7-8-37-48",
+			"kwh": 0.0,
+			"power": 0.0,
+			"startTime": "2019-9-5-18-31-29"
+		}, {
+			"endTime": "2019-9-8-22-44-8",
+			"kwh": 0.0,
+			"power": 0.0,
+			"startTime": "2019-9-7-8-37-48"
+		}, {
+			"endTime": "2019-9-10-12-50-27",
+			"kwh": 0.0,
+			"power": 0.0,
+			"startTime": "2019-9-8-22-44-8"
+		}, {
+			"endTime": "2019-9-12-2-56-46",
+			"kwh": 0.0,
+			"power": 0.0,
+			"startTime": "2019-9-10-12-50-27"
+		}, {
+			"endTime": "2019-9-13-17-3-6",
+			"kwh": 0.0,
+			"power": 0.0,
+			"startTime": "2019-9-12-2-56-46"
+		}, {
+			"endTime": "2019-9-15-7-9-25",
+			"kwh": 0.0,
+			"power": 0.0,
+			"startTime": "2019-9-13-17-3-6"
+		}, {
+			"endTime": "2019-9-16-21-15-44",
+			"kwh": 0.0,
+			"power": 0.0,
+			"startTime": "2019-9-15-7-9-25"
+		}, {
+			"endTime": "2019-9-18-11-22-4",
+			"kwh": 0.0,
+			"power": 0.0,
+			"startTime": "2019-9-16-21-15-44"
+		}, {
+			"endTime": "2019-9-20-1-28-23",
+			"kwh": 0.0,
+			"power": 0.0,
+			"startTime": "2019-9-18-11-22-4"
+		}, {
+			"endTime": "2019-9-21-15-34-42",
+			"kwh": 469545.46537487296,
+			"power": 12322.32102107132,
+			"startTime": "2019-9-20-1-28-23"
+		}, {
+			"endTime": "2019-9-23-5-41-1",
+			"kwh": 567512.948855051,
+			"power": 14893.289905001373,
+			"startTime": "2019-9-21-15-34-42"
+		}, {
+			"endTime": "2019-9-24-19-47-21",
+			"kwh": 204122.89630530003,
+			"power": 5356.775234721389,
+			"startTime": "2019-9-23-5-41-1"
+		}, {
+			"endTime": "2019-9-26-9-53-40",
+			"kwh": 2932749.745355427,
+			"power": 76964.39748999143,
+			"startTime": "2019-9-24-19-47-21"
+		}, {
+			"endTime": "2019-9-27-23-59-59",
+			"kwh": 2826595.8205234255,
+			"power": 74178.5911392001,
+			"startTime": "2019-9-26-9-53-40"
+		}]
         };
     },
 
@@ -215,6 +347,14 @@ export default {
     },
     methods: {
         drawLine() {
+        let timeArr = [];
+        this.electricityTotalList.forEach(item =>{
+            timeArr.push(item.createTime);
+        });
+        let kwhArr = [];
+        this.powerTotalList.forEach(item =>{
+            kwhArr.push(Number(item.kwh));
+        });
             // 基于准备好的dom，初始化echarts实例
             let myChart = this.$echarts.init(document.getElementById('myChart'));
             let myChart2 = this.$echarts.init(document.getElementById('myChart2'));
@@ -226,6 +366,9 @@ export default {
                     data: ['安康市', '宝鸡市', '汉中市', '商洛市', '铜川市', '渭南市', '西安市', '延安市']
                 },
                 yAxis: {},
+                legend: {
+                    data:['机构分布']
+                },
                 series: [
                     {
                         itemStyle: {
@@ -239,20 +382,39 @@ export default {
             });
             // 绘制图表
             myChart2.setOption({
-                title: { text: '近30天用电量统计' },
-                tooltip: {},
-                xAxis: {
-                    data: ['安康市', '宝鸡市', '汉中市', '商洛市', '铜川市', '渭南市', '西安市', '延安市']
+                color: ['#3398DB'],
+                tooltip : {
+                    trigger: 'axis',
+                    axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                        type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                    }
                 },
-                yAxis: {},
-                series: [
+                grid: {
+                    left: '3%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: true
+                },
+                xAxis : [
                     {
-                        itemStyle: {
-                            normal: { color: 'rgba(45,140,240,1)' }
-                        },
-                        name: '销量',
-                        type: 'bar',
-                        data: [1, 1, 0, 0, 0, 3, 0, 0]
+                        type : 'category',
+                        data : timeArr,
+                        axisTick: {
+                            alignWithLabel: true
+                        }
+                    }
+                ],
+                yAxis : [
+                    {
+                        type : 'value'
+                    }
+                ],
+                series : [
+                    {
+                        name:'用电量统计',
+                        type:'bar',
+                        barWidth: '60%',
+                        data:kwhArr
                     }
                 ]
             });
@@ -283,38 +445,6 @@ export default {
 .grid-num {
     font-size: 30px;
     font-weight: bold;
-}
-
-.grid-con-icon {
-    font-size: 50px;
-    width: 100px;
-    height: 100px;
-    text-align: center;
-    line-height: 100px;
-    color: #fff;
-}
-
-.grid-con-1 .grid-con-icon {
-    background: rgb(45, 140, 240);
-}
-
-.grid-con-1 .grid-num {
-    color: #fff;
-}
-
-.grid-con-2 .grid-con-icon {
-    background: rgb(100, 213, 114);
-}
-
-.grid-con-2 .grid-num {
-    color: #fff;
-}
-
-.grid-con-3 .grid-con-icon {
-    background: rgb(242, 94, 67);
-}
-
-.grid-con-3 .grid-num {
     color: #fff;
 }
 
