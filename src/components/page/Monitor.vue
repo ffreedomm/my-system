d7302252eafdd8605bb9b764eab5473718dd243f
Freedom <40904131+ffreedomm@users.noreply.github.com>
2019-10-02 16:55:47 +0800

    监控修改

--- src/components/page/Monitor.vue
+++ src/components/page/Monitor.vue
@@ -19,7 +19,7 @@
                         <div id="myChart" :style="{width: '100%', height: '200px'}"></div>
                     </el-row>
                     <el-row>
-                        <el-table :data="tData">
+                        <el-table :data="tData" height="320">
                             <el-table-column fixed="left" type="index" width="50" label="序号" align="center" />
                             <el-table-column prop="org.name" label="机构名称"/>
                             <el-table-column prop="deviceSum" label="设备数量" width="100"/>
@@ -29,8 +29,6 @@
                 </el-col>
                 <el-col :span="15">
                     <el-row :gutter="40">
-                        <!-- <div class="map">
-                        </div> -->
                            <baidu-map  class="map" 
                                 :zoom="zoom" :center="center"
                                 @ready="handler" 
@@ -48,13 +46,91 @@
                                 </bm-marker>
                                 <bm-navigation anchor="BMAP_ANCHOR_TOP_RIGHT"/>
                                 <bm-city-list anchor="BMAP_ANCHOR_TOP_LEFT"/>
-                                <bm-view 
-                                    :style="{width:'100%',height: '400px',flex: 1,marginBottom:'-30px'}"/>
+                                <!-- <bm-view 
+                                    :style="{width:'100%',height: '600px',flex: 1,marginBottom:'-30px'}"/> -->
                                 </baidu-map>
                     </el-row>
-                    <el-row :gutter="40">
-                        <div class="table"></div>
+                </el-col>
+            </el-row>
+            <el-divider/>
+            <el-row :gutter="40">
+                <el-col :span="9">
+                    <el-row :gutter="20" class="mgb20">
+                        <el-col :span="8">
+                            <el-card
+                                shadow="always"
+                                :body-style="{padding: '0px','background-color': 'lightskyblue'}"
+                            >
+                                <div slot="header" class="clearfix">
+                                    <div
+                                        style="color:blue;font-size:16px;font-weight:bold;margin:-18px -20px; padding:10px 10px; background: #f3f3f3;"
+                                    >总报警数</div>
+                                </div>
+                                <div class="grid-content grid-con-1">
+                                    <div class="grid-cont-right">
+                                        <div class="grid-num">{{warningData.total}}</div>
+                                    </div>
+                                </div>
+                            </el-card>
+                        </el-col>
+                        <el-col :span="8">
+                            <el-card
+                                shadow="hover"
+                                :body-style="{padding: '0px','background-color': '#94d6da'}"
+                            >
+                                <div slot="header" class="clearfix">
+                                    <div
+                                        style="color:green;font-size:16px;font-weight:bold;margin:-18px -20px; padding:10px 10px; background: #f3f3f3;"
+                                    >已处理数</div>
+                                </div>
+                                <div class="grid-content grid-con-2">
+                                    <div class="grid-cont-right">
+                                        <div class="grid-num">{{warningData.handled}}</div>
+                                        <span class="percent">{{warningData.handledPercent}}%</span>
+                                    </div>
+                                </div>
+                            </el-card>
+                        </el-col>
+                        <el-col :span="8">
+                            <el-card
+                                shadow="hover"
+                                :body-style="{padding: '0px','background-color': '#f3704b'}"
+                            >
+                                <div slot="header" class="clearfix">
+                                    <div
+                                        style="color:red;font-size:16px;font-weight:bold;margin:-18px -20px; padding:10px 10px; background: #f3f3f3;"
+                                    >未处理数</div>
+                                </div>
+                                <div class="grid-content grid-con-2">
+                                    <div class="grid-cont-right">
+                                        <div class="grid-num">{{warningData.unhandled}}</div>
+                                        <span class="percent">{{warningData.unhandledPercent}}%</span>
+                                    </div>
+                                </div>
+                            </el-card>
+                        </el-col>
                     </el-row>
+                        <el-divider>报警条数</el-divider>
+                        <el-tabs type="border-card" header-style="font-size: 12px;">
+                            <el-tab-pane>
+                                <span slot="label">1小时内(1条)</span>
+                            </el-tab-pane>
+                            <el-tab-pane>
+                                <span slot="label">8小时内(3条)</span>
+                            </el-tab-pane>
+                            <el-tab-pane>
+                                <span slot="label">24小时内(6条)</span>
+                            </el-tab-pane>
+                            <el-table>
+                                <el-table-column></el-table-column>
+                            </el-table>
+                        </el-tabs>
+                </el-col>
+                <el-col :span="10">
+                    <div id="myChart2" :style="{width: '100%', height: '400px'}"></div>
+                </el-col>
+                <el-col :span="5">
+                    <div id="myChart3" :style="{width: '100%', height: '400px'}"></div>
                 </el-col>
             </el-row>
         </div>
@@ -89,6 +165,7 @@ export default {
             zoom: 10,
             center:{lng: 108.89, lat: 34.34},
             markers:[],
+            warningData:{},
             tData:[],
             cData:[],
             timeId:{}
@@ -128,8 +205,12 @@ export default {
             this.getTData();
             //地图标点
             this.getMapPoint();
+            //报警处理
+            this.getWarningData();
             //饼图数据
             this.getCircelData();
+            //
+            this.initZXChart();
         },
         //下拉框数据
         getOrgList(){
@@ -178,11 +259,23 @@ export default {
                             this.cData.push({value:item.deviceSum,name:item.org.name});
                         })
                         this.initChart()
+                        this.initCChart();
                     }
                 }
             })
         },
 
+        //报警处理
+        getWarningData(){
+            getTerminalAlertTotalInFaultStatus().then(res=>{
+                if(res.success){
+                    if(res.object){
+                        this.warningData = res.object
+                    }
+                }
+            })
+        },
+        
         //转为树
         toTree(data) {
             let result = []
@@ -262,6 +355,90 @@ export default {
                 ]
             });
         },
+
+        initCChart(){
+            let myChart3 = this.$echarts.init(document.getElementById('myChart3'));
+            myChart3.setOption({
+                title: {
+                    text: '实时状态统计'
+                },
+                tooltip : {
+                    position: 'inner',
+                    trigger: 'item',
+                    formatter: "{a} <br/>{b} : {c} ({d}%)"
+                },
+                series : [
+                    {
+                        name: '',
+                        type: 'pie',
+                        radius : '75%',
+                        center: ['45%', '50%'],
+                        data:this.cData,
+                        itemStyle: {
+                            emphasis: {
+                                shadowBlur: 10,
+                                shadowOffsetX: 0,
+                                shadowColor: 'rgba(0, 0, 0, 0.5)'
+                            },
+                            normal:{ 
+                                label:{ 
+                                    show: true, 
+                                    formatter: '{a} \n{b}\n{c} ({d}%)' 
+                                }, 
+                                labelLine :{show:true} 
+                            }
+                        },
+                        label: {
+                            normal: {
+                                position: 'inner'
+                            }
+                        },
+                        labelLine :{show:true} 
+                    }
+                ]
+            });
+        },
+
+        //折线图
+        initZXChart(){
+            let myChart2 = this.$echarts.init(document.getElementById('myChart2'));
+            myChart2.setOption({
+                tooltip: {
+                    trigger: 'axis'
+                },
+                legend: {
+                    data:['电流值','功率值'],
+                    align: 'left'
+                },
+                grid: {
+                    left: '3%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: true
+                },
+                xAxis: {
+                    type: 'category',
+                    boundaryGap: false,
+                    data: ['周一','周二','周三','周四','周五','周六','周日']
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: [
+                    {
+                        name:'电流值',
+                        type:'line',
+                        data:[120, 132, 101, 134, 90, 230, 210]
+                    },
+                    {
+                        name:'功率值',
+                        type:'line',
+                        data:[820, 932, 901, 934, 1290, 1330, 1320]
+                    }
+                ]
+            });
+
+        },
     },
     filters:{
         //设置一个函数来进行过滤
@@ -284,10 +461,6 @@ export default {
 
 
 <style scoped>
-.map{
-    height:400px;
-    width:100%;
-}
 .table{
     height:400px;
     width:100%;
@@ -298,9 +471,36 @@ export default {
 
 .map {
  width: 100%;
- height: 400px;
+ height: 520px;
 }
 .el-select el-select--small{
     width: 35% !important;
+}
+
+.grid-content {
+    display: flex;
+    height: 80px;
+}
+
+.grid-cont-right {
+    margin-left: 10px;
+    margin-top: 8px;
+    flex: 1;
+    text-align: start;
+    font-size: 14px;
+    color: #999;
+}
+
+.grid-num {
+    font-size: 26px;
+    font-weight: bold;
+    color: #fff;
+}
+.percent{
+    font-size: 18px;
+    float: right;
+    margin-right: 12px;
+    margin-top: 12px;
+    color: #fff
 }
 </style>
