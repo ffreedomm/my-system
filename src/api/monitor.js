64e87408767d50104bfc6952c2c15b44d67481d0
Freedom <40904131+ffreedomm@users.noreply.github.com>
2019-10-02 14:53:13 +0800

    监控页

--- src/api/monitor.js
+++ src/api/monitor.js
@@ -1,32 +1,56 @@
 import request from '../utils/request';
 
 /**
- * 基本情况统计
+ * 机构信息
  */
-export const getBasicInfoTotalForToday = () => {
+export const getQueryOrgList = (data) => {
     return request({
-        url: '/BasicInfoTotalForToday',
+        url: '/QueryOrgList',
+        method: 'post',
+        data
+    })
+}
+
+/**
+ * 列表信息
+ */
+export const getAllTerminalTotalListForOrg = (orgId) => {
+    return request({
+        url: '/AllTerminalTotalListForOrg?orgId='+orgId,
         method: 'post'
     })
 }
 
 /**
- * 机构分布
+ * 地图标点查询
  */
-export const getOrgInZoneTotalList = () => {
+export const getAllDeviceListForOrg = (orgId) => {
     return request({
-        url: '/OrgInZoneTotalList',
+        url: '/AllDeviceListForOrg?orgId='+orgId,
         method: 'post'
     })
 }
 
 /**
- * 近30天用电量统计
+ * 饼图数据
+ * @param {} orgId 
  */
-export const getPETotalForAll = () => {
+export const getTerminalTotalListForOrgOrChild = (orgId) => {
     return request({
-        url: '/PETotalForAll?pointSum=20',
+        url: '/TerminalTotalListForOrgOrChild?orgId='+orgId,
         method: 'post'
     })
 }
+
+/**
+ * 设备报警处理
+ * @param {} orgId 
+ */
+export const getTerminalAlertTotalInFaultStatus = () => {
+    return request({
+        url: '/TerminalAlertTotalInFaultStatus',
+        method: 'post'
+    })
+}
+
 
--- src/components/page/Monitor.vue
+++ src/components/page/Monitor.vue
@@ -2,39 +2,59 @@
     <div>
         <div class="container">
             <el-row>
-                <el-select v-model="value" filterable placeholder="请选择">
-                    <el-option
-                    v-for="item in options"
-                    :key="item.value"
-                    :label="item.label"
-                    :value="item.value">
-                    </el-option>
-                </el-select>
+                <el-col :span="9">
+                <treeselect style="width:90%" 
+                    v-model="orgId" 
+                    :options="orgList"
+                    default-expand-all="true"
+                    placeholder="机构信息" />
+                </el-col>
+                <el-col :span="15">
+                    {{weather}} &nbsp; &nbsp; &nbsp; {{ date | formaDate }}
+                </el-col>
             </el-row>
             <el-row :gutter="40">
-                <el-col :span="12">
+                <el-col :span="9">
                     <el-row>
-                        <div id="myChart" :style="{width: '100%', height: '50%'}"></div>
+                        <div id="myChart" :style="{width: '100%', height: '200px'}"></div>
                     </el-row>
                     <el-row>
+                        <el-table :data="tData">
+                            <el-table-column fixed="left" type="index" width="50" label="序号" align="center" />
+                            <el-table-column prop="org.name" label="机构名称"/>
+                            <el-table-column prop="deviceSum" label="设备数量" width="100"/>
+                            <el-table-column prop="terminalSum" label="终端数量" width="100"/>
+                        </el-table>
+                    </el-row>
+                </el-col>
+                <el-col :span="15">
+                    <el-row :gutter="40">
+                        <!-- <div class="map">
+                        </div> -->
+                           <baidu-map  class="map" 
+                                :zoom="zoom" :center="center"
+                                @ready="handler" 
+                                :scroll-wheel-zoom="true"
+                                @click="clickEvent"
+                                ak="%E6%82%A8%E7%9A%84%E5%AF%86%E9%92%A5">
+                                <bm-marker 
+                                v-for="marker in markers" :key="marker.id"
+                                    :position="{lng: marker.longitude, lat: marker.latitude}" 
+                                    animation="BMAP_ANIMATION_BOUNCE">
+                                <bm-label 
+                                    :content="marker.name" 
+                                    :labelStyle="{color: 'red', fontSize : '15px'}" 
+                                    :offset="{width: 20, height: -20}"/>
+                                </bm-marker>
+                                <bm-navigation anchor="BMAP_ANCHOR_TOP_RIGHT"/>
+                                <bm-city-list anchor="BMAP_ANCHOR_TOP_LEFT"/>
+                                <bm-view 
+                                    :style="{width:'100%',height: '400px',flex: 1,marginBottom:'-30px'}"/>
+                                </baidu-map>
+                    </el-row>
+                    <el-row :gutter="40">
                         <div class="table"></div>
                     </el-row>
-                </el-col>
-                <el-col :span="12">
-                    <div class="map"></div>
-                </el-col>
-            </el-row>
-            <el-row>
-                <el-col :span="12">
-                    <el-row>
-                        <div id="myChart" :style="{width: '100%', height: '50%'}"></div>
-                    </el-row>
-                    <el-row>
-                        <div class="table"></div>
-                    </el-row>
-                </el-col>
-                <el-col :span="12">
-                    <div class="map"></div>
                 </el-col>
             </el-row>
         </div>
@@ -42,94 +62,245 @@
 </template>
 
 <script>
-import bus from '../common/bus';
+import {BaiduMap,BmNavigation,BmView,BmGeolocation,BmCityList,BmMarker,BmLabel} from 'vue-baidu-map'
+import Treeselect from '@riophae/vue-treeselect'
+import '@riophae/vue-treeselect/dist/vue-treeselect.css'
+import {getQueryOrgList,getAllTerminalTotalListForOrg,getAllDeviceListForOrg,getTerminalTotalListForOrgOrChild,
+getTerminalAlertTotalInFaultStatus} from '@/api/monitor';
+
 export default {
     name: 'monitor',
+　　components: {
+    BaiduMap,
+    BmNavigation,
+    BmView,
+    BmCityList,
+    BmMarker,
+    BmLabel,
+    Treeselect
+  },
+
     data() {
         return {
-            options: [{
-          value: '选项1',
-          label: '黄金糕'
-        }, {
-          value: '选项2',
-          label: '双皮奶'
-        }, {
-          value: '选项3',
-          label: '蚵仔煎'
-        }, {
-          value: '选项4',
-          label: '龙须面'
-        }, {
-          value: '选项5',
-          label: '北京烤鸭'
-        }],
-        value: ''
+            date:new Date(),
+            weather:'',
+            orgId:1,
+            orgList:[],
+            zoom: 10,
+            center:{lng: 108.89, lat: 34.34},
+            markers:[],
+            tData:[],
+            cData:[],
+            timeId:{}
         };
     },
-
+    watch: {
+      orgId(val) {
+        this.orgId = val;
+        this.initData();
+      }
+    },
     computed: {
         
     },
+    beforeDestroy:function(){
+        //实例销毁前青出于定时器
+        if(this.timeId){
+            clearInterval(this.timeId);
+        }
+    },
     mounted() {
-        this.initChart()
+        //创建定时器更新时间
+        var _this = this;
+        this.timeId= setInterval(function(){
+            _this.date = new Date();
+        },1000);
+        this.initWeather();
+        this.initData();
     },
     methods: {
+        //初始化数据
+        initData(){
+            this.cData = [];
+            //获取机构
+            this.getOrgList();
+            //列表
+            this.getTData();
+            //地图标点
+            this.getMapPoint();
+            //饼图数据
+            this.getCircelData();
+        },
+        //下拉框数据
+        getOrgList(){
+            const req = {start:1,end:99999}
+            getQueryOrgList(req).then(res=>{
+                if(res.success){
+                    if(res.object && res.object.length > 0){
+                        this.orgList = this.toTree(res.object);
+                    }
+                }
+            })
+        },
+        //饼图下表列表数据
+        getTData(){
+            getAllTerminalTotalListForOrg(this.orgId).then(res=>{
+                if(res.success){
+                    if(res.object && res.object.length > 0){
+                        this.tData = res.object
+                    }
+                }
+            })
+        },
+        //地图标点数据
+        getMapPoint(){
+            getAllDeviceListForOrg(this.orgId).then(res=>{
+                if(res.success){
+                    if(res.object && res.object.length > 0){
+                        this.markers = res.object
+                        if(this.orgId != 1){
+                            this.center={lng: this.markers[0].longitude, lat: this.markers[0].latitude};
+                       }else{
+                            this.center={lng: 108.89, lat: 34.34};
+                       }
+                        //this.handler()
+                    }
+                }
+            })
+        },
+
+        //获取饼图数据
+        getCircelData(){
+            getTerminalTotalListForOrgOrChild(this.orgId).then(res=>{
+                if(res.success){
+                    if(res.object && res.object.length > 0){
+                        res.object.forEach((item) =>{
+                            this.cData.push({value:item.deviceSum,name:item.org.name});
+                        })
+                        this.initChart()
+                    }
+                }
+            })
+        },
+
+        //转为树
+        toTree(data) {
+            let result = []
+            if(!Array.isArray(data)) {
+                return result
+            }
+            data.forEach(item => {
+                delete item.children;
+            });
+            let map = {};
+            data.forEach(item => {
+                map[item.id] = item;
+            });
+            data.forEach(item => {
+                item.label=item.name;
+                let parent = map[item.parentId];
+                if(parent) {
+                    (parent.children || (parent.children = [])).push(item);
+                } else {
+                    result.push(item);
+                }
+            });
+            return result;
+        },
+
+        initWeather(){
+            var weatherArr = ["晴，30°C", "多云，26°C", "小雨，21°C"];
+            var rand = Math.random();
+            rand = parseInt(rand * 10, 10) % 3;
+            this.weather = weatherArr[rand];
+        },
+
+        //初始化地图
+        handler ({BMap, map}) {
+            window.map = map;
+        },
+        //点击地图监听
+        clickEvent(e){
+            
+        },
         initChart(){
             let myChart = this.$echarts.init(document.getElementById('myChart'));
             myChart.setOption({
-                // title : {
-                //     //text: '某站点用户访问来源',
-                //     //subtext: '纯属虚构',
-                //     //x:'center'
-                // },
                 tooltip : {
+                    position: 'inner',
                     trigger: 'item',
                     formatter: "{a} <br/>{b} : {c} ({d}%)"
                 },
-                //legend: {
-                    //orient: 'vertical',
-                    //left: 'left',
-                    //data: ['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
-                //},
                 series : [
                     {
-                        name: '访问来源',
+                        name: '',
                         type: 'pie',
-                        radius : '55%',
-                        center: ['50%', '60%'],
-                        data:[
-                            {value:335, name:'直接访问'},
-                            {value:310, name:'邮件营销'},
-                            {value:234, name:'联盟广告'},
-                            {value:135, name:'视频广告'},
-                            {value:1548, name:'搜索引擎'}
-                        ],
+                        radius : '75%',
+                        center: ['45%', '50%'],
+                        data:this.cData,
                         itemStyle: {
                             emphasis: {
                                 shadowBlur: 10,
                                 shadowOffsetX: 0,
                                 shadowColor: 'rgba(0, 0, 0, 0.5)'
+                            },
+                            normal:{ 
+                                label:{ 
+                                    show: true, 
+                                    formatter: '{a} \n{b}\n{c} ({d}%)' 
+                                }, 
+                                labelLine :{show:true} 
                             }
-                        }
+                        },
+                        label: {
+                            normal: {
+                                position: 'inner'
+                            }
+                        },
+                        labelLine :{show:true} 
                     }
                 ]
             });
+        },
+    },
+    filters:{
+        //设置一个函数来进行过滤
+        formaDate(value){
+            //创建一个时间日期对象
+            var date = new Date();
+            var year = date.getFullYear();      //存储年
+            var month = date.getMonth()+1 <10 ? '0'+date.getMonth()+1 : date.getMonth()+1;    //存储月份
+            var day = date.getDate() <10 ? '0'+date.getDate() : date.getDate();//存储日期
+            var hours = date.getHours() <10 ? '0'+date.getHours() : date.getHours();      //存储时
+            var minutes = date.getMinutes() <10 ? '0'+date.getMinutes() : date.getMinutes();  //存储分
+            var seconds = date.getSeconds() <10 ? '0'+date.getSeconds() : date.getSeconds();  //存储秒
+            var week = "    星期" + "日一二三四五六".charAt(date.getDay());
+            //返回格式化后的日期
+            return year+'年'+month+'月'+day+'日'+hours+'时'+minutes+'分'+seconds+'秒'+week;
         }
-    }
+    },
 };
 </script>
 
 
 <style scoped>
 .map{
-    height:200px;
+    height:400px;
     width:100%;
-    background:red
 }
 .table{
-    height:100%;
+    height:400px;
     width:100%;
-    background:red
+    margin-top:20px;
+    background:blue
 
+}
+
+.map {
+ width: 100%;
+ height: 400px;
+}
+.el-select el-select--small{
+    width: 35% !important;
 }
 </style>
