3305c7e41d6018b456053f162aefaf29cd34beee
Freedom <40904131+ffreedomm@users.noreply.github.com>
2019-10-05 16:53:12 +0800

    用电统计

---
+++ src/api/electricity.js
@@ -0,0 +1,22 @@
+import request from '../utils/request';
+
+/**
+ * 修改
+ */
+export const forTotal = (data, url) => {
+    return request({
+        url: '/PETotalFor'+url,
+        method: 'post',
+        data,
+        transformRequest: [function (data) {
+            let ret = ''
+            for (let it in data) {
+              ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+            }
+            return ret
+        }],
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        }
+    })
+}
